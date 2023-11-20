b6b26e1ad25939d2afe75f95366e0810a1c88d5a
infiniteoo <troydorman@gmail.com>
2023-11-20 13:03:46 -0700

    created our main page templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import { About, Contact, Home, Projects } from "./pages";
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/projects" element={<h1>Projects</h1>} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>

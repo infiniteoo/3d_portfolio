60bd83282d9ae2d3d5f05c4fab62103ec399cdc8
infiniteoo <troydorman@gmail.com>
2023-11-20 12:40:48 -0700

    installed tailwind and implemented router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/projects" element={<h1>Projects</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
+      </Router>
+    </main>
+  );
 };
 
 export default App;

caa433aa21c1b86ab3d15b2ad88bacd8328e7e3e
infiniteoo <troydorman@gmail.com>
2023-11-20 12:55:50 -0700

    implemented navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
       <Router>
-        Navbar
+        <Navbar />
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/about" element={<h1>About</h1>} />

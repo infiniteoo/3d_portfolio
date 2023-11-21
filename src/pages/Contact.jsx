8574d1a115762e3cd13660cfc2d404fc47c60fcd
infiniteoo <troydorman@gmail.com>
2023-11-20 22:58:29 -0700

    dynamic animations based on form actions

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,33 +1,29 @@
-import { useState, useRef, Suspense } from "react";
 import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
+
 import Fox from "../models/Fox";
+
 import Loader from "../components/Loader";
 
 const Contact = () => {
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const formRef = useRef(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+  const [loading, setLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
   };
 
-  const handleFocus = (e) => {
-    e.target.classList.add("focus");
-  };
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
 
-  const handleBlur = (e) => {
-    e.target.classList.remove("focus");
-  };
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setIsLoading(true);
+    setLoading(true);
+    setCurrentAnimation("hit");
 
     emailjs
       .send(
@@ -35,35 +31,53 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Troy",
+          to_name: "JavaScript Mastery",
           from_email: form.email,
-          to_email: "troydorman@gmail.com",
+          to_email: "sujata@jsmastery.pro",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
-      .then(() => {
-        setIsLoading(false);
-        // TODO: show success message
-        // TODO: hide an alert
-        formRef.current.reset();
-        setForm({
-          name: "",
-          email: "",
-          message: "",
-        });
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        console.log(error);
-        // TODO: show error message
-      });
+      .then(
+        () => {
+          setLoading(false);
+          showAlert({
+            show: true,
+            text: "Thank you for your message 😃",
+            type: "success",
+          });
+
+          setTimeout(() => {
+            hideAlert(false);
+            setCurrentAnimation("idle");
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          }, [3000]);
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+          setCurrentAnimation("idle");
+
+          showAlert({
+            show: true,
+            text: "I didn't receive your message 😢",
+            type: "danger",
+          });
+        }
+      );
   };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
-        <h1 className="head-text">Get In Touch</h1>
+        <h1 className="head-text">Get in Touch</h1>
+
         <form
           ref={formRef}
           onSubmit={handleSubmit}
@@ -76,7 +90,7 @@ const Contact = () => {
               name="name"
               className="input"
               placeholder="John"
-              reqired
+              required
               value={form.name}
               onChange={handleChange}
               onFocus={handleFocus}
@@ -89,8 +103,8 @@ const Contact = () => {
               type="email"
               name="email"
               className="input"
-              placeholder="john@gmail.com"
-              reqired
+              placeholder="John@gmail.com"
+              required
               value={form.email}
               onChange={handleChange}
               onFocus={handleFocus}
@@ -99,29 +113,30 @@ const Contact = () => {
           </label>
           <label className="text-black-500 font-semibold">
             Your Message
-            <input
+            <textarea
               name="message"
+              rows="4"
               className="textarea"
-              rows={4}
-              placeholder="Let me know how I can help you"
-              reqired
+              placeholder="Write your thoughts here..."
               value={form.message}
               onChange={handleChange}
               onFocus={handleFocus}
               onBlur={handleBlur}
             />
           </label>
+
           <button
             type="submit"
+            disabled={loading}
             className="btn"
             onFocus={handleFocus}
             onBlur={handleBlur}
-            disabled={isLoading}
           >
-            {isLoading ? "Sending..." : "Send Message"}
+            {loading ? "Sending..." : "Submit"}
           </button>
         </form>
       </div>
+
       <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
         <Canvas
           camera={{
@@ -131,12 +146,21 @@ const Contact = () => {
             far: 1000,
           }}
         >
-          <directionalLight intensity={2.5} position={[0, 0, 1]} />
-          <ambientLight intensity={0.5} />
+          <directionalLight position={[0, 0, 1]} intensity={2.5} />
+          <ambientLight intensity={1} />
+          <pointLight position={[5, 10, 0]} intensity={2} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            intensity={2}
+          />
+
           <Suspense fallback={<Loader />}>
             <Fox
+              currentAnimation={currentAnimation}
               position={[0.5, 0.35, 0]}
-              rotation={[12.6, -0.6, 0]}
+              rotation={[12.629, -0.6, 0]}
               scale={[0.5, 0.5, 0.5]}
             />
           </Suspense>

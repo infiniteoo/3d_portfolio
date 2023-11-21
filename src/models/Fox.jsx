50fa286b8ade705c2513afd25bed3a551e496395
infiniteoo <troydorman@gmail.com>
2023-11-20 22:38:45 -0700

    added fox to contact page

--- src/models/Fox.jsx
+++ src/models/Fox.jsx
@@ -7,25 +7,61 @@ Source: https://sketchfab.com/3d-models/fox-f372c04de44640fbb6a4f9e4e5845c78
 Title: Fox
 */
 
-import React, { useRef } from 'react'
-import { useGLTF, useAnimations } from '@react-three/drei'
+import { a } from "@react-spring/three";
+import foxScene from "../assets/3d/fox.glb";
 
-export function Model(props) {
-  const group = useRef()
-  const { nodes, materials, animations } = useGLTF('/fox.glb')
-  const { actions } = useAnimations(animations, group)
+import { useRef, useEffect } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+
+const Fox = ({ currentAnimation, ...props }) => {
+  const group = useRef();
+  const foxRef = useRef();
+  //
+  const { gl, viewport } = useThree();
+
+  const { nodes, materials, animations } = useGLTF(foxScene);
+  const { actions } = useAnimations(animations, foxRef);
+
+  useEffect(() => {}, [actions, currentAnimation]);
+
   return (
-    <group ref={group} {...props} dispose={null}>
+    <a.group ref={group} {...props} dispose={null}>
       <group name="Sketchfab_Scene">
         <primitive object={nodes.GLTF_created_0_rootJoint} />
-        <skinnedMesh name="Object_7" geometry={nodes.Object_7.geometry} material={materials.PaletteMaterial001} skeleton={nodes.Object_7.skeleton} />
-        <skinnedMesh name="Object_8" geometry={nodes.Object_8.geometry} material={materials.PaletteMaterial001} skeleton={nodes.Object_8.skeleton} />
-        <skinnedMesh name="Object_9" geometry={nodes.Object_9.geometry} material={materials.PaletteMaterial001} skeleton={nodes.Object_9.skeleton} />
-        <skinnedMesh name="Object_10" geometry={nodes.Object_10.geometry} material={materials.PaletteMaterial001} skeleton={nodes.Object_10.skeleton} />
-        <skinnedMesh name="Object_11" geometry={nodes.Object_11.geometry} material={materials.PaletteMaterial001} skeleton={nodes.Object_11.skeleton} />
+        <skinnedMesh
+          name="Object_7"
+          geometry={nodes.Object_7.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_7.skeleton}
+        />
+        <skinnedMesh
+          name="Object_8"
+          geometry={nodes.Object_8.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_8.skeleton}
+        />
+        <skinnedMesh
+          name="Object_9"
+          geometry={nodes.Object_9.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_9.skeleton}
+        />
+        <skinnedMesh
+          name="Object_10"
+          geometry={nodes.Object_10.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_10.skeleton}
+        />
+        <skinnedMesh
+          name="Object_11"
+          geometry={nodes.Object_11.geometry}
+          material={materials.PaletteMaterial001}
+          skeleton={nodes.Object_11.skeleton}
+        />
       </group>
-    </group>
-  )
-}
+    </a.group>
+  );
+};
 
-useGLTF.preload('/fox.glb')
+export default Fox;
